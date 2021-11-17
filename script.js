5098b8012fb856a4430e0bbd4dc43aa00587bd37
ZeekBeakCreek <89820141+ZeekBeakCreek@users.noreply.github.com>
2021-11-16 17:37:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function checkQuiz() {
+  let score = 0;
+
+  if (document.getElementById('correct1').checked) {
+    score++;
+  } 
+  if (document.getElementById('correct2').checked) {
+    score++;
+  } 
+  if (document.getElementById('correct3').checked) {
+    score++;
+  } 
+  if (document.getElementById('correct4').checked) {
+    score++;
+  } 
+  if (document.getElementById('correct5').checked) {
+    score++;
+  } 
+
+  document.write(`Your score is ${score} / 5`)
+}
